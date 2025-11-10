a419112326ad1af3c64b43354d53f2553e5e19ac
aquintanas79 <aquintanas@unal.edu.co>
2025-11-10 18:11:38 -0500

    Create FileHandler.py

---
+++ Workshop_2/Initial Python Code/FileHandler.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class FileHandler(ABC):
+    @abstractmethod
+    def read(self, path):
+        pass
+
+    @abstractmethod
+    def write(self, path, data):
+        pass
+
+class JsonFileHandler(FileHandler):
+    def read(self, path):
+        with open(path, "r") as file:
+            return json.load(file)
+
+    def write(self, path, data):
+        with open(path, "w") as file:
+            json.dump(data, file, indent=4)
