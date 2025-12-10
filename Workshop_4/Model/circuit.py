9551375b2cf5ec3643be0d4950164823e3412f78
Juan Esteban Rincon Zambrano <jrinconz@unal.edu.co>
2025-12-09 22:19:38 -0500

    Update circuit.py

--- Workshop_4/Model/circuit.py
+++ Workshop_4/Model/circuit.py
@@ -1,1 +1,11 @@
+class Circuit:
+    def __init__(self):
+        self.components = []
+        self.connections = []
+
+    def add_component(self, component):
+        self.components.append(component)
+
+    def connect(self, source, target):
+        self.connections.append((source, target))
 
