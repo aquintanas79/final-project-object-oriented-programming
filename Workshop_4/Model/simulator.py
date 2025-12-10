0fb35f42326814266e4e252b38d24ac89af57d88
Juan Esteban Rincon Zambrano <jrinconz@unal.edu.co>
2025-12-09 22:19:51 -0500

    Update simulator.py

--- Workshop_4/Model/simulator.py
+++ Workshop_4/Model/simulator.py
@@ -1,1 +1,5 @@
-
+class Simulator:
+    def run(self, circuit):
+        for source, target in circuit.connections:
+            if hasattr(target, "update_state"):
+                target.update_state(source.state)
