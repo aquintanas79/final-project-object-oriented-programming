fa8a45f128e7bf533482a6aaf5b33dd5de21618e
Juan Esteban Rincon Zambrano <jrinconz@unal.edu.co>
2025-12-09 22:20:09 -0500

    Update file.manager.py

--- Workshop_4/Data/file.manager.py
+++ Workshop_4/Data/file.manager.py
@@ -1,1 +1,25 @@
+import json
+
+class FileManager:
+
+    def save(self, circuit, filename="circuit_design.json"):
+        data = {
+            "components": [c.name for c in circuit.components],
+            "connections": [(a.name, b.name) for a, b in circuit.connections]
+        }
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+
+    def load(self, circuit, filename="circuit_design.json"):
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        name_to_component = {}
+
+        for name in data["components"]:
+            name_to_component[name] = Component(name)
+            circuit.add_component(name_to_component[name])
+
+        for a, b in data["connections"]:
+            circuit.connect(name_to_component[a], name_to_component[b])
 
