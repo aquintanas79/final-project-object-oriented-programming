d0a63cf44eae62381d7cdc4cf676654b0af61133
Juan Esteban Rincon Zambrano <jrinconz@unal.edu.co>
2025-12-09 22:31:58 -0500

    Update main.py

--- Final.project.POO/main.py
+++ Final.project.POO/main.py
@@ -1,1 +1,17 @@
 
+from View.GUI import CircuitGUI
+from Model.circuit import Circuit
+from Data.file_manager import FileManager
+from PyQt5.QtWidgets import QApplication
+
+def main():
+    app = QApplication([])
+    circuit = Circuit()
+    file_manager = FileManager()
+
+    gui = CircuitGUI(circuit, file_manager)
+    gui.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
