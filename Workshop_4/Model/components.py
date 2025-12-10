4b342e16de651eb11f117f5e2608f7629bddba13
Juan Esteban Rincon Zambrano <jrinconz@unal.edu.co>
2025-12-09 22:19:27 -0500

    Update components.py

--- Workshop_4/Model/components.py
+++ Workshop_4/Model/components.py
@@ -1,1 +1,18 @@
+class Component:
+    def __init__(self, name):
+        self.name = name
+        self.state = False
+
+    def toggle(self):
+        pass
+
+
+class Switch(Component):
+    def toggle(self):
+        self.state = not self.state
+
+
+class Light(Component):
+    def update_state(self, signal):
+        self.state = signal
 
